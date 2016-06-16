95cda3881cae1d01d124af557abf93804a34bd17
Charles Skariah <charles2101+32@gmail.com>
2016-06-16 20:12:22 +0000

    adding_controller_logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'links#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -12,8 +12,9 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
+     resources :links
+    
+     get ':incoming_url' => 'links#show'
   # Example resource route with options:
   #   resources :products do
   #     member do
